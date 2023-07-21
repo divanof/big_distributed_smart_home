93adbfd38c1d1c0eb2e6d8eb6eb767a0c7a12f3d
divanof <d-ivanov04@mail.ru>
2023-07-21 14:54:31 +0300

    Updated database

--- web/bdsh_site/models.py
+++ web/bdsh_site/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class ESP(models.Model):
+    ip = models.GenericIPAddressField(null=True)
+
+
+class SensorData(models.Model):
+    """
+    sensor types:
+        1: light lamp
+        2: light sensor
+        3: reed
+        4: temperature sensor
+        5: humidity sensor
+        6: leakage sensor
+    """
+    sensor_type = models.IntegerField(default=-1)
+    value = models.FloatField()
+    board = models.ForeignKey(to=ESP, on_delete=models.CASCADE)
