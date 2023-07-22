11f2a2f467d2eb4ef2d59385941f289d46fa33df
divanof <d-ivanov04@mail.ru>
2023-07-22 11:53:55 +0300

    Reorganize folders

--- web/bdsh_site/serializers.py
+++
@@ -1,8 +0,0 @@
-from bdsh_site.models import SensorData
-from rest_framework import serializers
-
-
-class SensorDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SensorData
-        fields = ['id', 'sensor_type', 'value']
